cb158ee407a9472a4041086b9b8d818a4d4f2d6d
Monique <yofinancialfreedommatters@gmail.com>
2024-02-03 15:15:10 -0500

    Created carousel and fixed font switcher

--- exercises/week02/exercise02-attributes/main.js
+++ exercises/week02/exercise02-attributes/main.js
@@ -1,15 +1,29 @@
 function showCat() {
+    document.querySelector("#caption").innerHTML = "This is a cat";
+    document.querySelector("#current-image").src = "images/cat.jpg";
     console.log("This is a cat");
 }
 
 function showDog() {
     console.log("This is a dog");
+    document.querySelector("#caption").innerHTML = "This is a dog";
+    document.querySelector("#current-image").src = "images/dog.jpg";
 }
 
 function showBird() {
+    document.querySelector("#caption").innerHTML = "This is a bird";
+    document.querySelector("#current-image").src = "images/bird.jpg";
     console.log("This is a bird");
 }
 
 function showFish() {
+    document.querySelector("#caption").innerHTML = "This is a fish";
+    document.querySelector("#current-image").src = "images/fish.jpg";
     console.log("This is a fish");
+}
+
+function showMyHouse() {
+    document.querySelector("#caption").innerHTML = "This is my House";
+    document.querySelector("#current-image").src = "images/myhome.webp";
+    console.log("This is a my forever home");
 }
